5a16c222b5fcf7523dd5bd704200e0d89c699aaa
Adamin88 <elhihi.mohamed2gmail.com>
2024-05-31 01:50:46 +0000

    pasacal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,13 +6,13 @@ def pascal_triangle(n):
     """Triangle"""
     if n <= 0:
         return []
-    pas = [[1]]
+    triangle = [[1]]
     for row_number in range(1, n):
         row = [1]
         for j in range(1, row_number):
-            element = pas[row_number - 1][j - 1] + pas[row_number - 1][j]
+            element = triangle[row_number - 1][j - 1] + triangle[row_number - 1][j]
             row.append(element)
         row.append(1)
-        pas.append(row)
+        triangle.append(row)
 
-    return pas
+    return triangle
